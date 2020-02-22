713d69c2cba3ae9b3531ff0de0b0900c246a6860
GA <greenapplej16@gmail.com>
2020-02-22 19:30:13 +0900

    Dice 생성

---
+++ DiceRun/Assets/Scripts/RotatonKeyManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationKeyManager : MonoBehaviour
+{
+    public GameObject DiceObject;
+
+    public void LeftRotation()
+    {
+        
+    }
+
+
+
+}
---
+++ DiceRun/README.md
@@ -0,0 +1,3 @@
+# DiceRun
+
+달려라 주사위(DiceRun) 게임 개발
