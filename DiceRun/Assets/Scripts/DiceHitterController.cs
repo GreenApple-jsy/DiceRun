77475ae366853b11a951b290ee9d10ef9248ec99
GA <greenapplej16@gmail.com>
2020-02-27 18:46:37 +0900

    Road 자동 이동 및 위치 초기화, DiceHitter 숫자 변경

---
+++ DiceRun/Assets/Scripts/DiceHitterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceHitterController : MonoBehaviour
+{
+    private int myNumber;
+    private SpriteRenderer NumberSprite;
+    public Sprite[] DiceSprites = new Sprite[6];
+
+    void Start()
+    {
+        myNumber = Random.Range(1, 7);
+        NumberSprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        ChangeNumber();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //DiceHitter Number 변경
+    public void ChangeNumber()
+    {
+        int newNumber = Random.Range(1, 7);
+
+        while (newNumber == myNumber)
+        {
+            newNumber = Random.Range(1, 7);
+        }
+
+        myNumber = newNumber;
+
+        NumberSprite.sprite = DiceSprites[myNumber - 1];
+    }
+}
---
+++ DiceRun/Assets/Scripts/RoadManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour
+{
+    private float Speed;
+    private float EndPointZ;
+    private float StartPointZ;
+    private GameObject DiceHitterObject;
+
+    void Start()
+    {
+        DiceHitterObject = GameObject.Find("DiceHitter");
+        EndPointZ = (GameObject.Find("EndPointObject").transform.position.z);
+        StartPointZ = this.transform.position.z;
+        Speed = 0f;
+    }
+
+    void Update()
+    {
+        Speed += Time.deltaTime;
+
+        // z좌표 조금씩 이동
+        transform.Translate(0, 0, -1 * Speed * Time.deltaTime);
+
+        // 목표 지점에 도달했다면
+        if (transform.position.z <= EndPointZ)
+        {
+            DiceHitterObject.SetActive(false);
+            // 스타트 지점으로 이동
+            transform.Translate(0, 0, -1 * (EndPointZ - StartPointZ));
+
+            //DiceHitter Number 변경
+            DiceHitterObject.GetComponent<DiceHitterController>().ChangeNumber();
+
+            DiceHitterObject.SetActive(true);
+        }
+    }
+}
