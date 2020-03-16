4058840edddc00b6401968fa6f6ada6999ffec55
GA <greenapplej16@gmail.com>
2020-03-17 01:01:44 +0900

    StartMenu 추가

---
+++ DiceRun/Assets/Scripts/StartMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour
+{
+    public void StartButtonClick()
+    {
+        SceneManager.LoadScene("DiceRunMain");
+    }
+
+    public void OptionButtonClick()
+    {
+        //옵션 패널 열기
+    }
+
+    public void RankingButtonClick()
+    {
+        //랭킹 패널 열기
+    }
+
+
+    void Update()
+    {
+        //뒤로가기 키 처리
+    }
+
+    public void QuitButtonClick()
+    {
+        //종료
+        Application.Quit();
+    }
+}
